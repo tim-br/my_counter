519f2637209ff3eec1b237271102213d31009925
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2024-04-22 01:16:38 -0700

    Fixed reference to multiple global counters.

--- lib/my_counter_web/live/counter_live.ex
+++ lib/my_counter_web/live/counter_live.ex
@@ -3,25 +3,24 @@ defmodule MyCounterWeb.CounterLive do
 
   def mount(_params, session, socket) do
     Phoenix.PubSub.subscribe(GlobalCounter.PubSub, "my_counter_server:update")
-    init_value = GenServer.call({:global, :global_counter}, :get_value)
     subdomain = session["subdomain"] || "default"
+    init_value = GenServer.call({:global, String.to_atom(subdomain)}, :get_value)
     {:ok, assign(socket, %{count: init_value, subdomain: subdomain})}
   end
 
   def handle_event("add", _params, socket) do
     ## MyCounterServer.add()
-    GenServer.cast({:global, :global_counter}, :add)
+    GenServer.cast({:global, String.to_atom(socket.assigns.subdomain)}, :add)
     {:noreply, assign(socket, :count, socket.assigns.count)}
   end
 
   def handle_event("subtract", _params, socket) do
-    GenServer.cast({:global, :global_counter}, :subtract)
+    GenServer.cast({:global, String.to_atom(socket.assigns.subdomain)}, :subtract)
     {:noreply, assign(socket, :count, socket.assigns.count)}
   end
 
   @impl true
   def handle_info(update, socket) do
-    # Update the socket with the new information
     {:noreply, assign(socket, :count, update)}
   end
 end
